2750b56070439563d5108e7045026c737a79bff2
kiyani <bamba.kiyani@gmail.com>
2019-10-14 17:54:32 -0400

    Testing React

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import * as rtl from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+
+
+// before each step we clean up and wrappe our app
+
+afterEach(rtl.cleanup);
+
+let wrapper;
+
+beforeEach(() => {
+  wrapper = rtl.render(<App />);
+});
+
+describe('App component', () => {
+  test('matches the snapshot!', () => {
+    expect(wrapper.container).toMatchSnapshot();
+  });
 });
---
+++ src/Steps.js
@@ -0,0 +1,7 @@
+// 1. Installed our library
+    // Jest comes with CRA
+    // @testing-library/React
+    // @testing-library/jest-dom
+
+// 2. Clean up : delete react-dom and import our libraries in app.test.js 
+// QUESTION: Why do we delete react dom ?
